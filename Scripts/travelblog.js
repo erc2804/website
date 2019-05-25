da85312babac0367a5002b72757e69f5bea8ee63
erc2804 <erc2804@outlook.de>
2019-05-25 22:33:05 +0200

    removed tbModal fragments

--- Scripts/travelblog.js
+++ Scripts/travelblog.js
@@ -2,14 +2,6 @@ $(init);
 
 function init() {
   $(window).resize(checkNavMenu);
-  $(".portfolio-box > a").click(function() {
-    toggleTbModal();
-  });
-	$("#tb-modal-bg").click(function() {
-		if(checkTbModalActive()) {
-			toggleTbModal();
-		}
-  });
 }
 
 function checkNavMenu() {
@@ -27,17 +19,4 @@ function scrollToPortfolio() {
 function morphBurgerBtn() {
   $(".navIcon > div").toggleClass("animate");
   $(".navMenu").toggleClass("deepHide");
-}
-
-// ---
-function checkTbModalActive() {
-  return !$("#tb-modal").hasClass("deepHide");
-}
-
-function toggleTbModal() {
-  if (checkTbModalActive()) {
-    $("#tb-modal, #tb-modal-bg").addClass("deepHide");
-  } else {
-    $("#tb-modal, #tb-modal-bg").removeClass("deepHide");
-  }
 }
