3d31ceefde6fa67b9a0e32483a0141af5b4ad700
Ercan Cicek <ercan.cicek@infraview.net>
2019-11-14 21:15:29 +0100

    restructured base

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,53 @@
 import React, { Component } from "react"
+import { Link } from "gatsby"
+// --- styles
+import "../styles/globals.scss"
 import headerStyles from "../styles/header.module.scss"
-
+// --- images
+import logoImg from "../images/logo.svg"
 class Header extends Component {
   render() {
+    const navbarElements = [
+      {
+        label: "Portfolio",
+        path: "/portfolio",
+      },
+      {
+        label: "Blog",
+        path: "/blog",
+      },
+      {
+        label: "Travel",
+        path: "/travel",
+      },
+      {
+        label: "About Me",
+        path: "/about-me",
+      },
+      {
+        label: "Dev Tools",
+        path: "/dev-tools",
+      },
+    ]
     return (
-      <header className={[headerStyles.headerContainer, "bg-blue-dark"].join(" ")}>
-        Header
+      <header className={headerStyles.headerWrapper}>
+        <div className={headerStyles.headerContainer}>
+          <Link className={headerStyles.logoContainer} to="/">
+            <img src={logoImg} alt="logo" />
+          </Link>
+          <div className={headerStyles.navWrapper}>
+            {navbarElements.map((navElem, i) => (
+              <Link
+                className={headerStyles.navElement}
+                activeClassName={headerStyles.navActive}
+                key={i}
+                to={navElem.path}
+              >
+                {navElem.label}
+              </Link>
+            ))}
+          </div>
+        </div>
       </header>
     )
   }
