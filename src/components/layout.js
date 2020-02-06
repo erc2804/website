f09ce65e522fdbcaf18261f8340724a67c00a68e
erc2804 <erc2804@outlook.de>
2020-02-06 21:39:01 +0100

    added some meta tags

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,10 +10,17 @@ const Layout = props => {
     <div className={layoutStyles.masterWrapper}>
       <Helmet>
         <title>{props.title} - ercancicek.de</title>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0, user-scalable=no"
-        />
+        <meta charset="utf-8" />
+        <meta name="keywords" lang="de" content="ercan cicek, ux, designer, entwickler, developer, webdesigner, webentwickler, frontend-developer, mainz, mobile app developer" />
+        <meta name="description" lang="de" content="Ercan Cicek - UX-Developer, UX-Designer, Web-App-Developer, Webentwickler, Webdesigner in Mainz" />
+        <meta name="author" content="Ercan Cicek" />
+        <meta name="page-topic" lang="de" content="Dienstleistung, Person" />
+        <meta name="robots" content="index,follow" />
+        <meta name="revisit-after" content="30 days" />
+        <meta name="audience" content="alle" />
+        <meta name="expires" content="0" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" />
+        <meta http-equiv="imagetoolbar" content="false" />
       </Helmet>
       <div className={layoutStyles.masterContainer}>
         <Header mode={props.darkHeader ? 'dark' : ''}/>
